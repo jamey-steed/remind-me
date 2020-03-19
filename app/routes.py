e48c2e54587041c3d29667f251975d2029f94d32
Jamey Steed <jsteed@cefcu.com>
2020-03-19 13:20:24 -0500

    Adds requests and submit route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    return request.form
